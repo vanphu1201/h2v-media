4027e331ab99b5c8ea52e5d5afebe0bbdc32fcf0
vanphu1201 <vanphu120107@gmail.com>
2026-05-09 14:58:54 +0700

    Fix 404: disable cloudflare and cleanup nitro config

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,9 +1,16 @@
 import { defineNitroConfig } from "nitropack/config";
 
 export default defineNitroConfig({
+  srcDir: "src",
+  handlers: [
+    {
+      route: "/**",
+      handler: "./server.ts",
+    },
+  ],
   publicAssets: [
     {
-      dir: "dist/client",
+      dir: "../dist/client",
       maxAge: 31536000,
     },
   ],
